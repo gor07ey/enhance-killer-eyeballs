8ccc1755b84cd8f3c4866668857ab4e59e3d18f0
gor07ey <152075976+gor07ey@users.noreply.github.com>
2023-12-27 05:54:09 +0300

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,29 @@
+def generate_random_data():
+    main()
+if __name__ == "__main__":
+
+    return data
+
+    data = generate_random_data()
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+
+    for item in data:
+import random
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    return data;
+function main() {
+main();
+    data.forEach(item => {
+
+    }
+}
+
+        console.log("Random Number: " + item);
+    });
+function generateRandomData() {
+    const data = generateRandomData();
